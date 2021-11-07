e3da6398e2b047a2987da9a5acd7ef02ebe6f341
Norman Lawm <norman.ml.law@live.com>
2021-11-07 14:53:22 +0800

    Update forecast of forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (longitude, latitude, callback) => {
         //         feelslike: body.current.feelslike,
         //         timezone_id: body.location.timezone_id,
         //   });
-             callback(undefined, body.current.weather_descriptions[0] + ' today, temperature is ' +body.current.temperature + '°C, feelslike ' + body.current.feelslike + '°C. Timezone ID is ' +  body.location.timezone_id);
+             callback(undefined, body.current.weather_descriptions[0] + ' today, temperature is ' +body.current.temperature + '°C, feelslike ' + body.current.feelslike + '°C. Humidity is ' + body.location.humidity + '. Timezone ID is ' +  body.location.timezone_id);
         }
     });
 }
